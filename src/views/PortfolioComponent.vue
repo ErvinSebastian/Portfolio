d220c084b92fcd06b87215ab88bd28005815b637
Ervin Sebastian <ervin.sebastian.aesiph@gmail.com>
2023-10-28 00:36:34 +0800

    update

--- src/views/PortfolioComponent.vue
+++ src/views/PortfolioComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card lg:card-side bg-base-100 shadow-xl glass" id="projects">
-        <figure></figure>
+        <figure><img src="../assets/images/CovidTracker.png" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">Covid-19 Updates</h2>
             <p>A simple table that fetch updated statistics about Corona Virus Data API.</p>
