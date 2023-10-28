77db74ac9c847be13790501a19a459efdfd18461
Ervin Sebastian <ervin.sebastian.aesiph@gmail.com>
2023-10-28 12:46:45 +0800

    update

--- src/views/PortfolioComponent.vue
+++ src/views/PortfolioComponent.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="card lg:card-side bg-base-100 shadow-xl glass" id="projects">
+    <div id="projects">
+        <div class="card lg:card-side bg-base-100 shadow-xl glass" id="projects">
         <figure><img src="../assets/images/CovidTracker.png" alt="Shoes" /></figure>
         <div class="card-body">
             <h2 class="card-title">Covid-19 Updates</h2>
@@ -9,6 +10,32 @@
             </div>
         </div>    
     </div>
+    <div class="flex mt-16">
+        <div class="card w-96 bg-base-100 shadow-xl image-full m-2">
+        <figure><img src="../assets/images/coffee-bg.jpg" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Coming Soon</h2>
+            <p>A non friendly UX application created to have fun while learning new trending tech stack.</p>
+            <div class="card-actions justify-end">
+            <!-- <button class="btn btn-sm btn-primary"></button> -->
+            </div>
+        </div>    
+    </div>
+
+    <div class="card w-96 bg-base-100 shadow-xl image-full m-2">
+        <figure><img src="../assets/images/coffee-bg.jpg" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">Coming Soon</h2>
+            <p>A non friendly UX application created to have fun while learning new trending tech stack.</p>
+            <div class="card-actions justify-end">
+            <!-- <button class="btn btn-sm btn-primary"></button> -->
+            </div>
+        </div>    
+    </div>
+    </div>
+    
+    </div>
+  
 </template>
   
 <script>
