d666b66d98054fc9f18473ec4c038430419c9609
Ervin Sebastian <ervin.sebastian.aesiph@gmail.com>
2023-10-28 11:42:52 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <div>
         <p class="font-mono">Hi! My name is</p>
         <h1 class="text-5xl font-bold">Ervin Sebastian</h1>
-        <p class="py-6 font-mono">I solve modern problems with modern solutions by user-friendly web applications from scratch.</p>
+        <p class="py-6 font-mono">I love to solve business problems by providing user-friendly web applications and solutions from scratch.</p>
         <button class="btn btn-sm btn-outline btn-accent">Contact Me</button>
     </div>
 
